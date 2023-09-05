f31c471dff6910570723d59e0a81488066c29c5a
Bene-volent <benevolent733@gmail.com>
2023-09-05 14:36:38 +0530

    Landing Page Done

--- landing-page/src/scripts/scroll.js
+++
@@ -1,8 +0,0 @@
-const header = document.querySelector(".header")
-window.addEventListener("scroll",()=>{
-    if (window.scrollY < 125){
-        header.dataset.onTop = true;
-    }else{
-        header.dataset.onTop = false
-    }
-})
